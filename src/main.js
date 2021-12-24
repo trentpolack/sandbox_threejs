66e570e1ab44ea9ed55b46ca2a5173c85cba5606
trentpolack <trentp@gmail.com>
2021-12-24 00:52:15 -0500

    Checking in progress on ripping out window/browser/canvas-level management into a platform class. Or, more accurately, learning JS while trying and failing to do something like that.

--- src/main.js
+++ src/main.js
@@ -1,31 +1,39 @@
 import * as THREE from '../node_modules/three/build/three.module.js';
 //import { TrackballControls } from '../node_modules/three/examples/jsm/controls/TrackballControls.js';
+import { Platform } from './platform/platform.js';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 76.0, window.innerWidth/window.innerHeight, 0.1, 1000.0 );
+const camera = new THREE.PerspectiveCamera( 76.0, window.innerWidth / window.innerHeight, 0.1, 1000.0 );
+
+// Setup the platform instance.
+//const platform = new Platform( renderer.domElement );
+const platform = new Platform( document.querySelector( '#viewport ') );
 
 const renderer = new THREE.WebGLRenderer( {
-    antialias: true
+  canvas: platform.canvas,
+  antialias: true
 } );
 
-const setupRenderer = function( ) {
-    // Configure renderer and canvas.
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+const setupRenderer = function () {
+  // Configure renderer and canvas.
+  renderer.setSize( window.innerWidth, window.innerHeight );
+//  document.body.appendChild( renderer.domElement );
 
-    window.addEventListener( 'resize', () => {
-        renderer.setSize(window.innerWidth, window.innerHeight);
-        camera.aspect = window.innerWidth / window.innerHeight;
-        camera.updateProjectionMatrix();
-    } )
+  window.addEventListener( 'resize', () => {
+    if( platform.onResize( renderer ) ) {
+
+      camera.aspect = platform.getAspectRatio( );
+      camera.updateProjectionMatrix();
+    }
+  } )
 }
 
 // Setup some temp geometry.
 const geometry = new THREE.TorusKnotGeometry( 2.5, 0.1, 32, 16, 2, 3 );
-const material = new THREE.MeshStandardMaterial( { 
-    color: 0x00ff00,
-    roughness: 1.0,
-    metallic: 0.0
+const material = new THREE.MeshStandardMaterial( {
+  color: 0x00ff00,
+  roughness: 1.0,
+  metallic: 0.0
 } );
 
 // Add the actual mesh geometry to the scene.
@@ -39,20 +47,20 @@ scene.add( light );
 camera.position.z = 7.0;
 
 // Game tick update.
-const gameTick = function( ) {
-    requestAnimationFrame( gameTick );
+const gameTick = function () {
+  requestAnimationFrame( gameTick );
 
-    meshGeometry.rotation.x+= 0.01;
-    meshGeometry.rotation.y+= 0.01;
+  meshGeometry.rotation.x += 0.01;
+  meshGeometry.rotation.y += 0.01;
 
-    renderTick( );
+  renderTick();
 };
 
 // Renderer tick update.
-const renderTick = function( ) {
-    renderer.render( scene, camera );
+const renderTick = function () {
+  renderer.render( scene, camera );
 }
 
-setupRenderer( );
+setupRenderer();
 
-gameTick( );
+gameTick();
